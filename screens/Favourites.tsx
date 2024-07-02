7cad480e4e4e067e7d8b93983dff5ddc76b0359c
unknown <me@bzyhas.com>
2024-07-02 20:54:49 +0200

    Added tallying of favourites

--- screens/Favourites.tsx
+++ screens/Favourites.tsx
@@ -8,6 +8,7 @@ import { fetchDetails } from '../utils/get-meds';
 import Medication from '../components/Medications/Medication';
 import LoadingPage from '../components/UI/LoadingPage';
 import AdBanner from '../components/Ads/AdBanner';
+import { Colors } from '../constants/colors';
 
 export default function Favourites() {
   const [loading, setLoading] = useState<boolean>();
@@ -29,11 +30,31 @@ export default function Favourites() {
     return <LoadingPage />;
   }
 
+  let totalDDD = 0;
+  for (const med of favCtx.favMeds) {
+    totalDDD += Number(med.DDD);
+  }
+
+  let totalPrice = 0;
+  for (const med of favCtx.favMeds) {
+    totalPrice += Number(med.PrisPrPakning);
+  }
+
   return (
     <View style={styles.screen}>
       {favCtx.favMeds.length < 1 && (
         <Text style={styles.emptyText}>Ingen favoritter tilføjet endnu.</Text>
       )}
+      <View style={styles.tally}>
+        <View style={styles.tallyBox}>
+          <Text>Samlet døgnspris:</Text>
+          <Text style={styles.bold}>{totalDDD.toFixed(2)} kr.</Text>
+        </View>
+        <View style={styles.tallyBox}>
+          <Text>Samlet pakkepris:</Text>
+          <Text style={styles.bold}>{totalPrice.toFixed(2)} kr.</Text>
+        </View>
+      </View>
       <FlatList
         data={favCtx.favMeds}
         renderItem={(itemData) => <Medication {...itemData.item} />}
@@ -50,7 +71,30 @@ const styles = StyleSheet.create({
     marginHorizontal: 12,
     paddingVertical: 12,
   },
+  tally: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    gap: 8,
+  },
+  tallyBox: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: Colors.gray200,
+    marginTop: 8,
+    padding: 4,
+    backgroundColor: Colors.white,
+    borderRadius: 6,
+    elevation: 2,
+    shadowColor: 'black',
+    shadowOpacity: 0.15,
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+    alignItems: 'center',
+  },
   emptyText: {
     marginTop: 32,
+  },
+  bold: {
+    fontWeight: 'bold',
   },
 });
